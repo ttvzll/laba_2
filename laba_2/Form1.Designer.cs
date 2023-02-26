f0d54da1998b732b5156cec621b5cff6485fabe1
Liza <Liza@HOME-PC>
2023-02-26 23:42:51 +0300

    Добавьте файлы проекта.

---
+++ laba_2/Form1.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace laba_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.splitter1 = new System.Windows.Forms.Splitter();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.listBox1.Enabled = false;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(0, 0);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(293, 450);
+            this.listBox1.TabIndex = 0;
+            // 
+            // listBox2
+            // 
+            this.listBox2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listBox2.Enabled = false;
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(293, 0);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(507, 450);
+            this.listBox2.TabIndex = 1;
+            // 
+            // splitter1
+            // 
+            this.splitter1.Location = new System.Drawing.Point(293, 0);
+            this.splitter1.Name = "splitter1";
+            this.splitter1.Size = new System.Drawing.Size(3, 450);
+            this.splitter1.TabIndex = 2;
+            this.splitter1.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.splitter1);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Splitter splitter1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ laba_2/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Management;
+
+namespace laba_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            Drives();
+            timer1.Interval = 30000;
+            timer1.Enabled = true;
+        }
+        private void Drives()
+        {
+            listBox1.Items.Clear();
+            listBox2.Items.Clear();
+            DriveInfo[] driveInfos = DriveInfo.GetDrives();
+            foreach(DriveInfo driveInfo in driveInfos)
+            {
+                listBox2.Items.Add("Название диска: "+driveInfo.Name);
+                listBox2.Items.Add("Свободное место: "+driveInfo.TotalFreeSpace/1024/1024/1024+" Гб");
+            }
+            ObjectQuery wql = new ObjectQuery("SELECT * FROM Win32_OperatingSystem");
+            ManagementObjectSearcher searcher = new ManagementObjectSearcher(wql);
+            ManagementObjectCollection results = searcher.Get();
+
+            foreach (ManagementObject result in results)
+            {
+                listBox1.Items.Add($"  Общая видимая оперативная память: {Convert.ToDouble(result["TotalVisibleMemorySize"]) / (1024 * 1024):f2} Гб");
+                listBox1.Items.Add($"  Свободная физическая оперативная память: {Convert.ToDouble(result["FreePhysicalMemory"]) / (1024 * 1024):f2} Гб");
+                listBox1.Items.Add($"  Общая виртуальная оперативная память: {Convert.ToDouble(result["TotalVirtualMemorySize"]) / (1024 * 1024):f2} Гб");
+                listBox1.Items.Add($"  Свободная виртуальная оперативная память:  {Convert.ToDouble(result["FreeVirtualMemory"]) / (1024 * 1024):f2} Гб");
+            }
+
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Drives();
+
+        }
+    }
+}
+    
+
